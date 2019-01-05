6c1659982a0ce80da1bd85487c3de924580a3c96
Francesco Cogno <francesco.cogno@outlook.com>
2019-01-05 20:33:17 +0100

    Added params

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use actix;
 use actix_web::http::Method;
-use actix_web::{
-    http, middleware, pred, server, App, Error, HttpRequest, HttpResponse, Path, Responder,
-};
+use actix_web::{middleware, pred, server, App, Error, HttpRequest, HttpResponse};
+use clap;
+use clap::Arg;
 use futures::future::{result, FutureResult};
+use log::info;
 use std::env;
 
-// async handler
-fn index_async(req: &HttpRequest) -> FutureResult<HttpResponse, Error> {
-    println!("{:?}", req);
+fn metrics_handler(_req: &HttpRequest) -> FutureResult<HttpResponse, Error> {
+    //    println!("{:?}", req);
 
     result(Ok(HttpResponse::Ok()
         .content_type("text/plain")
@@ -16,23 +16,51 @@ fn index_async(req: &HttpRequest) -> FutureResult<HttpResponse, Error> {
 }
 
 fn main() {
-    env::set_var("RUST_LOG", "actix_web=debug");
+    let matches = clap::App::new("prometheus_dhcp_exporter")
+        .version("0.1")
+        .author("Francesco Cogno <francesco.cogno@outlook.com>")
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .help("exporter port (default 9979)")
+                .default_value("9979")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .help("verbose logging")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    if matches.is_present("verbose") {
+        println!("verbose logging enabled");
+        env::set_var("RUST_LOG", "actix_web=debug");
+    }
+    env_logger::init();
+
+    let bind = format!("127.0.0.1:{}", matches.value_of("port").unwrap());
+
     let sys = actix::System::new("prometheus_dhcp_exporter");
 
-    let addr = server::new(|| {
+    let _addr = server::new(|| {
         App::new()
             // enable logger
             .middleware(middleware::Logger::default())
-            .resource("/metrics", |r| r.method(Method::GET).a(index_async))
+            .resource("/metrics", |r| r.method(Method::GET).a(metrics_handler))
             .default_resource(|r| {
                 // all requests that are not `GET`
                 r.route()
                     .filter(pred::Not(pred::Get()))
-                    .f(|req| HttpResponse::MethodNotAllowed());
+                    .f(|_| HttpResponse::MethodNotAllowed());
             })
     })
-    .bind("127.0.0.1:9979")
+    .bind(bind.clone())
     .unwrap()
     .start();
+
+    info!("starting exporter on {}", bind);
+
     sys.run();
 }
